562e4ca81492a73f68b6c00254d7fb5294e86faf
github username <github email address>
2020-02-12 16:52:17 -0500

    added timetable sort

--- app/models/train.rb
+++ app/models/train.rb
@@ -2,4 +2,7 @@ class Train < ApplicationRecord
     has_many :arrivals
     has_many :stations, through: :arrivals 
     validates :name, uniqueness: { scope: :destination }
+    def sort_timetable
+        self.arrivals.sort_by{|arrival| arrival.arrival_time.to_i}
+    end
 end
