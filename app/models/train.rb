cf533b9d614fafe0f9f357afb1e00a36d78742e3
github username <github email address>
2020-02-23 14:00:55 -0500

    looking for train lin bug

--- app/models/train.rb
+++ app/models/train.rb
@@ -8,16 +8,22 @@ class Train < ApplicationRecord
     end
     
     def self.refresh
+        Rails.logger.info("Refreshing")
         response_string = RestClient.get('http://web.mta.info/status/serviceStatus.txt')
+        Rails.logger.info("Got response")
         response_string.gsub!("NQR","NQRW")
         response = JSON.parse(Hash.from_xml(response_string).to_json)
+        Rails.logger.info("Made it JSON")
         response["service"]["subway"]["line"][0..-2].each do |line|
+            Rails.logger.info("About to make lines")
             if line["text"] == nil
+                Rails.logger.info("Making good service line")
                 line["name"].split("").each{|indiv| 
                     train = Train.where(name: indiv)
                     train.update(name: indiv, status: line["status"])
                 }
             else
+                Rails.logger.info("Making delayed line")
                 line["text"] = line["text"].gsub("<br clear=left>"," ")
                 elaboration = Nokogiri::HTML(CGI.unescapeHTML(line["text"])).content.squish
                 line["name"].split("").each{|indiv| 
